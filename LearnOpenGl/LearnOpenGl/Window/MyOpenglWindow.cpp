4899cde1ce97eb979c0eb2b87b6de8b487e1c012
liliguang <liguang.li-a1154@aqara.com>
2020-09-13 17:38:45 +0800

    渐变层实现

--- LearnOpenGl/LearnOpenGl/Window/MyOpenglWindow.cpp
+++ LearnOpenGl/LearnOpenGl/Window/MyOpenglWindow.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <math.h>
 
 #include "MyShader.hpp"
 #include "MyProgram.hpp"
@@ -20,19 +21,35 @@
 
 // 模拟顶点数据
 //三角形
+//GLfloat triangleVertices[] = {
+//    -0.5f, -0.5f, 0.0f,  //左下
+//     0.5f, -0.5f, 0.0f,  //右下
+//     0.0f,  0.5f, 0.0f,   //中上
+//};
+
+//三角形
 GLfloat triangleVertices[] = {
-    -0.5f, -0.5f, 0.0f,  //左下
-     0.5f, -0.5f, 0.0f,  //右下
-     0.0f,  0.5f, 0.0f,   //中上
+    //顶点数据            //颜色数据
+    -0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f, //左下     红
+     0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, //右下     绿
+     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f, //中上     蓝
 };
   
 
+////正方形
+//GLfloat squareVertices[] = {
+//    -0.5f, -0.5f, 0.0f,     //左下
+//     0.5f, -0.5f, 0.0f,     //右下
+//     0.5f,  0.5f, 0.0f,     //右上
+//    -0.5f,  0.5f, 0.0f,     //左上
+//};
+
 //正方形
 GLfloat squareVertices[] = {
-    -0.5f, -0.5f, 0.0f,  //左下
-     0.5f, -0.5f, 0.0f,  //右下
-     0.5f,  0.5f, 0.0f,   //右上
-    -0.5f,  0.5f, 0.0f,   //左上
+    -0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  //左下     红
+     0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  //右下     绿
+     0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  //右上     绿
+    -0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  //左上     红
 };
   
   
@@ -42,25 +59,83 @@ unsigned int  squareIndices[] = { // 注意索引从0开始!
 };
 
 
-//顶点着色器程序
+///
+/// 默认的程序, 没有值得传递
+////顶点着色器程序
+//char *vertexShaderStr = SHADER(
+//    \#version 330 core\n
+//      layout (location = 0) in vec3 position;
+//      void main()
+//      {
+//          gl_Position = vec4(position.x, position.y, position.z, 1.0);
+//      }
+//);
+//
+////片元着色器程序
+//char *fragmentShaderSrc = SHADER(
+//      \#version 330 core\n
+//      out vec4 color;
+//      void main()
+//      {
+//          color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+//      }
+//);
+
+
+
+///
+/// 着色器程序之间的数据传递
 char *vertexShaderStr = SHADER(
     \#version 330 core\n
-                               layout (location = 0) in vec3 position;
-                               void main()
-                               {
-    gl_Position = vec4(position.x, position.y, position.z, 1.0);
+                               
+    layout (location = 0) in vec3 position; //顶点数据源输入
+    layout (location = 1) in vec3 color; //颜色数据源输入
+                               
+    out vec4 vertexColor;//把片元着色器的颜色从这里输出
+                               
+    void main()
+    {
+        gl_Position = vec4(position, 1.0f);
+        vertexColor = vec4(color, 1.0f); //输出给片元着色器
     }
 );
 
 //片元着色器程序
 char *fragmentShaderSrc = SHADER(
-            \#version 330 core\n
-                                 out vec4 color;
-                                 void main()
-                                 {
-    color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    \#version 330 core\n
+    in vec4 vertexColor;//从顶点着色器中拿color的值
+    out vec4 color;
+    void main()
+    {
+      color = vertexColor;//获取值
     }
 );
+
+
+///
+/// 着色器Uniform
+//char *vertexShaderStr = SHADER(
+//    \#version 330 core\n
+//    layout (location = 0) in vec3 position;
+//    void main()
+//    {
+//        gl_Position = vec4(position.x, position.y, position.z, 1.0);
+//    }
+//);
+//
+////片元着色器程序
+//char *fragmentShaderSrc = SHADER(
+//    \#version 330 core\n
+//    uniform vec4 ourColor;
+//    out vec4 color;
+//    void main()
+//    {
+//      color = ourColor;//获取值
+//    }
+//);
+
+
+
 
 
 
@@ -96,40 +171,13 @@ int runMyOpenGlWindow() {
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
     
     
-    
-    
      //----------------------------------------------------------------------
     //先创建我们的Program对象, 加载顶点着色器程序和片元着色器程序
     MyProgram myProgram = MyProgram(vertexShaderStr, fragmentShaderSrc);
     
     
+   
     
-    
-    //程序有了, 接着处理数据
-
-    /*
-     VBO
-     ------------------------
-     | -------------------- |
-     | |VAO               | |
-     | -------------------- |
-     | -------------------- |
-     | |VAO               | |
-     | -------------------- |
-     | -------------------- |
-     | |VAO               | |
-     | -------------------- |
-     | -------------------- |
-     | |VAO               | |
-     | -------------------- |
-     |  ....                |
-     |  ....                |
-     |                      |
-     | -------------------- |
-     | |EBO               | |
-     | -------------------- |
-     ------------------------
-     */
     GLuint VBO , VAO , EBO;
     unsigned int squareIndicesCount = 0;
     showTriangle = false;
@@ -143,8 +191,15 @@ int runMyOpenGlWindow() {
        //创建VAO
         glGenVertexArrays(1, &VAO);
         glBindVertexArray(VAO);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+        //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+        //glEnableVertexAttribArray(0);
+        
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
         glEnableVertexAttribArray(0);
+        
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+        glEnableVertexAttribArray(1);
+        
         //解绑VAO
         glBindVertexArray(0);
         
@@ -155,12 +210,17 @@ int runMyOpenGlWindow() {
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(squareVertices), squareVertices, GL_STATIC_DRAW);
 
-        
         //创建VAO
         glGenVertexArrays(1, &VAO);
         glBindVertexArray(VAO);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+        //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+        //glEnableVertexAttribArray(0);
+        
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
         glEnableVertexAttribArray(0);
+        
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+        glEnableVertexAttribArray(1);
         
         //创建EBO, 这里的EBO相当于索引的作用
         glGenBuffers(1, &EBO);
@@ -169,26 +229,23 @@ int runMyOpenGlWindow() {
 
         //解绑VAO
         glBindVertexArray(0);
-        
         //计算索引个数
         squareIndicesCount = sizeof(squareIndices)/sizeof(squareIndices[0]);
-
-
         
     }
     
    
     
     
-    //在glUseProgram函数调用之后，每个着色器调用和渲染调用都会使用这个程序对象（也就是之前写的着色器)了。
-    glUseProgram(myProgram.program);
+   
+     
     
-    //线框模式
-//    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
+    
+     //线框模式
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     //----------------------------------------------------------------------
+  
  
-
 
     
     //进行绘制
@@ -199,19 +256,30 @@ int runMyOpenGlWindow() {
         //渲染指令
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        //在glUseProgram函数调用之后，每个着色器调用和渲染调用都会使用这个程序对象（也就是之前写的着色器)了。
+        glUseProgram(myProgram.program);
 
-        //这里VAO也行, VBO也行
-        glBindVertexArray(VBO);
+
+//        使用Uniform方式的代码
+//        GLfloat timeValue = glfwGetTime();
+//        GLfloat greenValue = (sin(timeValue) / 2 )  + 0.5;
+//        GLint vertexColorLocation = glad_glGetUniformLocation(myProgram.program, "ourColor");
+//        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+//        glUseProgram(myProgram.program);
+        
+        
+        glBindVertexArray(VAO);
         
         if (showTriangle) {
             glDrawArrays(GL_TRIANGLES, 0, 3);
             
         } else {
             //使用EBO
-            //glDrawElements(GL_TRIANGLES, squareIndicesCount, GL_UNSIGNED_INT, 0);
+            glDrawElements(GL_TRIANGLES, squareIndicesCount, GL_UNSIGNED_INT, 0);
             
             //不使用EBO
-            glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
+//            glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
             /*
             v参数1：有三种取值
             1.GL_TRIANGLES：每三个顶之间绘制三角形，之间不连接
@@ -223,8 +291,6 @@ int runMyOpenGlWindow() {
         
         //主要渲染函数
         glBindVertexArray(0);
-        
-        
         
         //交换缓冲
         glfwSwapBuffers(window);
