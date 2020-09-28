44d490deb12568dd56147192a9c1bc69783b1c57
liliguang <liliguang@followme.cn>
2020-09-28 17:10:21 +0800

    摄像机Demo修改

--- LearnOpenGl/LearnOpenGl/Demo/GETSTART/CAMERA/MyCamera.cpp
+++ LearnOpenGl/LearnOpenGl/Demo/GETSTART/CAMERA/MyCamera.cpp
@@ -76,7 +76,7 @@ int runMyCameraCube() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     
-    data = stbi_load( "/Users/lumi/Desktop/LearnOpengl/LearnOpenGl/LearnOpenGl/Demo/Common/ImgSources/dizhuan.jpg" , &width, &height, &nrChannels, 0);
+    data = stbi_load( "/Users/liliguang/Desktop/LearnOpengl/LearnOpenGl/LearnOpenGl/Demo/Common/ImgSources/dizhuan.jpg" , &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -92,46 +92,65 @@ int runMyCameraCube() {
     
     glEnable(GL_DEPTH_TEST);
 
-
+    
+    glm::vec3 cubePositions[2];
+    for (int i = 0; i<2; i++) {
+        if (i == 0) {
+            cubePositions[i] =glm::vec3( 0.0f,  0.0f,  0.0f);
+        } else {
+            float xRandomNum = arc4random_uniform(10.0) - 5.0f;
+            float yRandomNum = arc4random_uniform(10.0) - 5.0f;
+            cubePositions[i] =glm::vec3( xRandomNum,  yRandomNum,  0.0f);
+        }
+    }
+    
     //进行绘制
     while(!glfwWindowShouldClose(window)){
-       //检查事件
         glfwPollEvents();
         
-        //渲染指令
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glUseProgram(myProgram.program);
 
-        //加载纹理
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture);
         glUniform1i(glGetUniformLocation(myProgram.program, "myTexture"), 0);
         
-        //单位矩阵
+        
         glm::mat4 model = glm::mat4(1.0f);
         glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 projection = glm::mat4(1.0f);
         
-        ///变换处理
         GLint myModelLoc = glGetUniformLocation(myProgram.program,"myModel");
         GLint myViewLoc = glGetUniformLocation(myProgram.program,"myView");
         GLint myProjectionLoc = glGetUniformLocation(myProgram.program,"myProjection");
         
-        model = glm::rotate(model, glm::radians(-45.0f), glm::vec3(1.0f,0.0f,0.0f));//以x轴旋转45度
-        view = glm::translate(view, glm::vec3(0.0f,0.0f, -3.0f)); // 向Z轴的负方向移动
-        view = glm::rotate(view,(GLfloat)glfwGetTime() * 1.0f, glm::vec3(1.0f, 1.0f, 0.0f));
         projection = glm::perspective(glm::radians(60.0f), 1.0f, 0.01f, 100.f);//投影矩阵
-
-        glUniformMatrix4fv(myModelLoc, 1, GL_FALSE, glm::value_ptr(model));
-        glUniformMatrix4fv(myViewLoc, 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(myProjectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
-
         
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, squareIndicesCount);
+        for(unsigned int i = 0; i < 2; i++)
+        {
+            //改变模型数据的位置
+            model = glm::mat4(1.0f);
+            model = glm::translate(model,glm::vec3(cubePositions[i].x,cubePositions[i].y,0.0f));//x,y平移
+            glUniformMatrix4fv(myModelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+            // 观察矩阵用视摄像机视角,也就是旋转摄像机
+            GLfloat radius = 10.0f;
+            GLfloat camX = sin(glfwGetTime()) * radius;
+            GLfloat camZ = cos(glfwGetTime()) * radius;
+            view = glm::lookAt(glm::vec3(camX, 0.0f, camZ),
+                               glm::vec3(0.0f, 0.0f, 0.0f),
+                               glm::vec3(0.0f, 1.0f, 0.0f));
+            
+            glUniformMatrix4fv(myViewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+            glDrawArrays(GL_TRIANGLES, 0, squareIndicesCount);
+
+        }
         glBindVertexArray(0);
-        //交换缓冲
+
         glfwSwapBuffers(window);
     }
 
